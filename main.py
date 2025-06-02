9c507275e43b37b2224d6cdea9371742ae962185
boundm <fsdljfsdjnaknasdn@proton.me>
2025-06-02 14:11:47 +0400

    add __init.py__

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import run
+import asyncio
+import sys
+
+from bot.loader import bot, dp
+from bot.handlers import user
+
+async def main():
+    dp.include_router(user.router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
